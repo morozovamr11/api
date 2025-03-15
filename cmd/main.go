ff10d051d1d679d0d1c82fb6f6db419aacc976e5
Marina <morozovamr11@gmail.com>
2025-03-15 21:21:16 +0500

    config go

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"api/internal/hello"
 	"fmt"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Println("hello")
-}
-
 func main() {
 	router := http.NewServeMux()
-	router.HandleFunc("/hello", hello)
+	hello.NewHelloHandler(router)
 	server := http.Server{
-		Addr:    "8081",
+		Addr:    ":8081",
 		Handler: router,
 	}
 	fmt.Println("Server is listening on port 8081")
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err)
+	}
 	//http.ListenAndServe(":8081", nil) //используется дефолтный server max
 }
