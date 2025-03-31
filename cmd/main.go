7d249e74f740c425cbcb5f1d95042f4bd75a744c
Marina <morozovamr11@gmail.com>
2025-04-01 00:24:28 +0500

    добавление логирования с помощью middleware

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"api/internal/auth"
 	"api/internal/link"
 	"api/pkg/db"
+	"api/pkg/middleware"
 	"fmt"
 	"net/http"
 )
@@ -28,7 +29,7 @@ func main() {
 
 	server := http.Server{
 		Addr:    ":8081",
-		Handler: router,
+		Handler: middleware.Logging(router), //использование middleware
 	}
 	fmt.Println("Server is listening on port 8081")
 	err := server.ListenAndServe()
---
+++ pkg/middleware/logs.go
@@ -0,0 +1,19 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func Logging(next http.Handler) http.Handler { //передается что-то имеющее метод ServeHTTP(ResponseWriter, *Request) например router := http.NewServeMux()
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		wrapper := &WrapperWriter{
+			ResponseWriter: w,
+			StatusCode:     http.StatusOK, //default значение
+		}
+		next.ServeHTTP(wrapper, r)                                               //вместо w http.ResponseWriter передали модифицированнный wrapper чтобы сохранить код
+		log.Println(wrapper.StatusCode, r.Method, r.URL.Path, time.Since(start)) //!используется пакет log в log.Println
+	})
+}
